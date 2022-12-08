441eaf420b90f2fd7fbc6600aed586f3d6760db1
Gracias KPAKOSSOU <gracias.kpakossou@>
2022-12-08 17:44:35 +0100

    commt

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express') 
 const morgan = require('morgan'); 
 const bodyParser = require('body-parser');
+const { required } = require('nodemon/lib/config');
 const app = express();
  app.set('port', (process.env.PORT || 3000));
 app.use(morgan('dev')); 
 app.use(bodyParser.urlencoded({ extended:false })); 
 app.use(bodyParser.json());
- nécessitent ('./routes/webhook_verify') (app);
+ require ('./routes/webhook_verify') (app);
 app.listen(app.get('port'), function() { 
   const url = 'http://localhost:' + app.set('port'); 
   console.log("Application en cours d'exécution sur le port : ", app.get('port'));
